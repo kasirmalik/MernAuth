c8103f18555ee41527cb1eb5da70968598255a7c
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-01-14 22:10:33 +0530

    Add type definitions and compatibility exports for RxJS modules

---
+++ backend/mailtrap/mailtrap.js
@@ -0,0 +1,33 @@
+import { MailtrapClient } from "mailtrap-client";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+const ENDPOINT = process.env.MAILTRAP_ENDPOINT;
+
+const client = new MailtrapClient({
+  endpoint: ENDPOINT, 
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "kasirmalik87@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
