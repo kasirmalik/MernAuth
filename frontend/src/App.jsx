4ba0bb1b3b96d5d025b8fad5af900ef9f99126f5
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-01-18 15:02:16 +0530

    Add SignUp and Login pages with routing and input components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import FloatingShape from './components/FloatingShape';
-import {Routes,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import SignUp from './pages/SignUp';
+import LoginPage from './pages/LoginPage';
+
 
 function App() {
 
   return (
-    <>
+    <Router>
       <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center
       relative overflow-hidden">
 
@@ -14,14 +17,13 @@ function App() {
         <FloatingShape  color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={"Home"} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
 
-
       </div>
-    </>
+    </Router>
   )
 }
 
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,65 @@
 
+import { motion } from 'framer-motion';
+import Input from '../components/Input';
+import {Mail, User,Lock} from 'lucide-react';
+import { useState } from 'react';
+
 
 const SignUp = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignUp = (e) => {
+        e.preventDefault()
+    }
   return (
-    <div>
-      signup
-    </div>
+    <motion.div
+       initial={{opacity:0,y:20}}
+       animate={{opacity:1,y:0}}
+       transition={{duration:0.5}}
+       className='max-w-md w-full bg-gray-500 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
+
+    >
+        <div className='p-8'>
+            <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 bg-clip-text' >
+                Create Account
+            </h2>
+
+            <form onSubmit={handleSignUp}>
+                <Input 
+                icon={User}
+                type='text'
+                placeholder='full name'
+                value={name}
+
+                onChange={(e) => setName(e.target.value)}
+                />
+                <Input 
+                icon={Mail}
+                type='email'
+                placeholder='email'
+                value={email}
+
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input 
+                icon={Lock}
+                type='password'
+                placeholder='password'
+                value={password}
+
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                {/* password strength meter */}
+
+                <motion.button >
+                    Sign Up
+                </motion.button>
+            </form>
+        </div>
+      
+    </motion.div>
   )
 }
 
